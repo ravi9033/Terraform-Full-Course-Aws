d875688a0e7180b201500ac08824c478dceb7c4b
Ravindra Maghudiya <rmaghudiya@gmail.com>
2025-12-01 17:20:56 +0530

    updates

--- lessons/day02/main.tf
+++ lessons/day02/main.tf
@@ -5,13 +5,6 @@ resource "aws_vpc" "ravindra_vpc" {
     }
 }
 
-resource "resource_type" "my_resource" {
-  #Configuration options
-  #Resource arguments
-  instance_type = "t2.micro"
-  ami           = "ami-0c55b159cbfafe1f0"
-}
-
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-0c55b159cbfafe1f0"
